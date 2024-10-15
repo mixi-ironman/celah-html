f5d120cd45e9086197a605e6c59896d967515e39
unknown <phamvannam.dev@gmail.com.com>
2024-10-15 15:42:56 +0700

    .

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,6 +2,8 @@
 $(document).ready(function () {
     // Slider chính
     $('.slider-list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         dots: true,
         infinite: true,
         speed: 500,
@@ -11,6 +13,138 @@ $(document).ready(function () {
             "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
         nextArrow:
             "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    $('.category-list').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        autoplay: false,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    $('.slick-banner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        autoplay: false,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    $('.slick-flash_sale').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        autoplay: false,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    $('.slick-bargain').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: true,
+        autoplay: false,
+        prevArrow:
+            "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+        nextArrow:
+            "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     });
 
     // Slider hiển thị nhiều item
